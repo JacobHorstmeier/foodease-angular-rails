22dae53d0f26748eaa7d5f9cf5914d75664284f4
Jesse Novotny <jessenovotny@gmail.com>
2016-11-16 13:14:09 -0700

    listing all cookbook.ingredients and shopping_list.ingredients. added toggle to add/remove from each

--- app/assets/javascripts/controllers/ShoppingListController.js
+++ app/assets/javascripts/controllers/ShoppingListController.js
@@ -4,10 +4,28 @@ angular
     var ctrl = this
     Auth.currentUser().then(function(user) {
       ctrl.user = user
-      // debugger;
       $scope.ingredients = ctrl.user.cookbook.ingredients
-      $scope.shoppingList = ctrl.user.shopping_list
+      $scope.shoppingList = ctrl.user.shopping_list.ingredients
+      // debugger;
       ctrl.pagination = Pagination.getNew(10);
       ctrl.pagination.numPages = Math.ceil($scope.ingredients.length/ctrl.pagination.perPage);
     })
+
+    ctrl.addToShoppingList = function(ingredient){
+      ingredient.added = true;
+      // debugger;
+      $scope.shoppingList.push(ingredient);
+
+    }
+
+    ctrl.removeFromShoppingList = function(ingredient){
+      ingredient.added = false;
+      var list = $scope.shoppingList
+      for(var i = 0; i < list.length; i++){
+        if(list[i] === ingredient){
+          // debugger;
+          $scope.shoppingList.splice(i, 1)
+        }
+      }
+    }
   })
