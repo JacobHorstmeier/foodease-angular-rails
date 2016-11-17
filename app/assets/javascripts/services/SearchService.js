a3f122a520801b9c5fa0031f6997bf92fa06b8a8
Jesse Novotny <jessenovotny@gmail.com>
2016-11-17 12:31:14 -0700

    adjusted amount of max recipes from search and pagination page toggle

--- app/assets/javascripts/services/SearchService.js
+++ app/assets/javascripts/services/SearchService.js
@@ -2,7 +2,7 @@
   function SearchService($http){
     this.getRecipes = function(query, user){
       var url = 'https://api.edamam.com/search?q=' + query;
-      url += '&from=0&to=20'
+      url += '&from=0&to=50'
       url += '&callback=angular.callbacks._0'
       if(user){
         user.healthLabels.forEach(function(label){
