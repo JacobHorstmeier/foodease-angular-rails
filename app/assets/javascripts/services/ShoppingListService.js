d3b2444b9143f358d16c921322342052630edbaf
Jesse Novotny <jessenovotny@gmail.com>
2016-11-16 22:46:26 -0700

    forgot to direct inject for ShoppingListService. Oops

--- app/assets/javascripts/services/ShoppingListService.js
+++ app/assets/javascripts/services/ShoppingListService.js
@@ -1,9 +1,16 @@
-angular
-  .module('reciPlease')
-  .service('ShoppingListService', function($http){
+(function(){
+  function ShoppingListService ($http){
 
     this.updateShoppingList = function(method, shoppingListId, ingredientId){
       var url = '/shopping_lists/' + shoppingListId + '/ingredients/' + ingredientId;
       return $http({url: url, method: method})
     }
-  })
+  }
+
+  ShoppingListService.$inject = ['$http']
+
+  angular
+  .module('reciPlease')
+  .service('ShoppingListService', ShoppingListService)
+}())
+
