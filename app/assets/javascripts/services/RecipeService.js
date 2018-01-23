150ba969283eb27ecbfc91aad03050f7862b732c
Jesse Novotny <jessenovotny@gmail.com>
2018-01-23 11:51:02 -0700

    added app id and key to query

--- app/assets/javascripts/services/RecipeService.js
+++ app/assets/javascripts/services/RecipeService.js
@@ -5,6 +5,7 @@
     this.getRecipes = function(query, user){
       var url = 'https://api.edamam.com/search?q=' + query;
       url += '&from=0&to=50'
+      url += '&app_id=10d42f85&app_key=eebfb150fdd04e3418cd3fb620e91387'
       if(user){
         user.healthLabels.forEach(function(label){
           url += '&health=' + label.label
